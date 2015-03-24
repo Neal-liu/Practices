60092ea220b7943695dfac25dd20bd3e6c99b71c
Neal <hunter0hunter04@hotmail.com.tw>
2015-03-24 20:10:50 +0800

    test remote

---
+++ t.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+ 
+int main(void)
+{
+    int x = 0x00000400;
+
+	for (i = 0; i < x; i++) {
+		/* code */
+	}
+	printf("hello x = %d\n",x);
+
+	return 0;
+}
+
+int fibonacci(void){
+	
+	return;
+}
