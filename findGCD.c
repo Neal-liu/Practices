168fc0ff0848591cac00f711116b1ee2a935d4a6
Neal-liu <hunter0hunter04@hotmail.com.tw>
2015-08-11 01:02:30 +0000

    more quick to find GCD.

--- findGCD.c
+++ findGCD.c
@@ -4,13 +4,16 @@
 
 int findGCD(int, int);
 int findGCD2(int, int);
+int findGCD3(int, int);
 
 int main(void)
 {
 	int result1 = findGCD(A,B);
 	int result2 = findGCD2(A,B);
+	int result3 = findGCD3(A,B);
 	printf("GCD is %d\n", result1);
 	printf("GCD is %d\n", result2);
+	printf("GCD is %d\n", result3);
 
 	return 0;
 }
@@ -42,3 +45,33 @@ int findGCD2(int a, int b)
 			return 1;
 	}
 }
+
+int findGCD3(int a, int b)
+{
+	while (1) {
+		if (a > (b * 4)) {
+			a %= b;
+			if (a == 0) return b;
+			if (a == 1) return 1;
+
+		} 
+		else if (a >= b) {
+			a -= b;
+			if (a == 0) return b;
+			if (a == 1) return 1;
+		}
+		if (b > (a * 4)) {
+			b %= a;
+			if (b == 0) return a;
+			if (b == 1) return 1;
+
+		}
+		else if (b >= a) {
+			b -= a;
+			if (b == 0) return a;
+			if (b == 1) return 1;
+		}
+	}
+
+}
+
