a58467eba534e242915d5ed752ad6ac8621b374d
Neal-liu <hunter0hunter04@hotmail.com.tw>
2015-08-11 00:54:27 +0000

    use  operator to implements.

--- findGCD.c
+++ findGCD.c
@@ -3,11 +3,15 @@
 #define B 18
 
 int findGCD(int, int);
+int findGCD2(int, int);
 
 int main(void)
 {
-	int result = findGCD(A,B);
-	printf("GCD is %d\n", result);
+	int result1 = findGCD(A,B);
+	int result2 = findGCD2(A,B);
+	printf("GCD is %d\n", result1);
+	printf("GCD is %d\n", result2);
+
 	return 0;
 }
 
@@ -20,5 +24,21 @@ int findGCD(int a, int b)
 			b -= a;
 		else
 			return a;
+	}
+}
+
+int findGCD2(int a, int b)
+{
+	while(1){
+		a %= b;
+		if(a == 0)
+			return b;
+		if(a == 1)
+			return 1;
+		b %= a;
+		if(b == 0)
+			return a;
+		if(b == 1)
+			return 1;
 	}
 }
