4cc3d37f1bcfaef4e71bee5b799e46764615a124
Neal-liu <nealliu.tw@gmail.com>
2016-05-11 20:51:25 +0800

    implements concatenate files.

---
+++ cat.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	FILE *fp;
+	void filecopy(FILE *, FILE *);
+
+	if(argc == 1){	// no arguments
+		filecopy(stdin, stdout);
+	}
+	else{
+		while(--argc > 0){
+			if((fp = fopen(*++argv, "r")) == NULL){
+				printf("Can not open file!\n");
+				return 1;
+			}
+			else{
+				filecopy(fp, stdout);
+				fclose(fp);
+			}
+		}
+	}
+
+	return 0;
+}
+
+/* copy file ifp to file ofp. */
+void filecopy(FILE *ifp, FILE *ofp)
+{
+	char c;
+	while((c = getc(ifp)) != EOF){
+		putc(c, ofp);
+	}
+}
